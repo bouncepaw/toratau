e160799ba7310027f667c34a52fb74fe89172f38
Timur Ismagilov <bouncepaw2@ya.ru>
2019-11-05 18:36:36 +0500

    Add helpful comments

--- expand.scm
+++ expand.scm
@@ -1,12 +1,14 @@
 ;; macro.scm
 ;; Expanding macros in Toratau files
+;; TODO: better naming
 
 (module toratau.expand
   *
   (import scheme
           (chicken base)
           (srfi 13)
-          (srfi 1))
+          (srfi 1)
+          )
 
   ;; separated-src is List<List|String>. If an element is String, it is kept as is, else it is expanded and the result of expanding is kept. Next, everything saved is joined together.
   (define (expand-string str)
@@ -19,26 +21,31 @@
            separated-src)
       ""))
 
-  ;; 
+  ;;
   (define (expand-list expression)
     (cond
       ((atom? expression) (expand-atom expression))
       (else
         )))
 
-  ;; Strings wrapped with in '' or {} or unwrapped at are left untouched. Others (in "") are expanded first.
+  ;; :: String -> String
+  ;; Strings wrapped with in '' or {} are left unwrapped. Others (in "") are expanded.
   (define (expand-atom atom)
     (define (first+last str)
       (let ((chars (string->list str)))
         (list (car chars) (last chars))))
     (define (bare str)
       (let ((chars (string->list str)))
-        (drop-right (cdr chars) 1)))
+        (list->string (drop-right (cdr chars) 1))))
     (if (filter
           (lambda (x) (apply equal? x))
-          (zip (circular-list (first+last atom))
-               (map first+last (list "{}" "''"))))
+          (zip (map first+last '("{}" "''"))
+               (circular-list (first+last atom))))
         (bare atom)
         (expand-string atom)))
 
+  ;; :: String -> List<List|String>
+  ;; Breaks up source text to some kind of tokens.
+  ;; TODO: implement.
+  (define (str->separated-src str) (list str))
 )
